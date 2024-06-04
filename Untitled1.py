4018a240ce2ce45cee72ef3cdb41d3de241c2690
TrdanhTg <93244826+TrdanhTg@users.noreply.github.com>
2024-06-04 18:29:23 +0700

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[4]:
+
+
+df = pd.read_csv('DS_Store.csv', encoding='iso-8859-1')
+df.head()
+
+
+# In[ ]:
+
+
+
+
